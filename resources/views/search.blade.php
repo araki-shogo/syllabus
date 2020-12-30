73956b3be50d962b70b5197f51ae19edfbf036ef
araki_shogo <araki@crane-inc.io>
2020-12-30 22:54:50 +0900

    css移動

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,58 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <title>小柏ゼミ | 授業検索</title>
-    <style>
-        .tabs {
-        margin-top: 50px;
-        padding-bottom: 40px;
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        width: 700px;
-        margin: 0 auto;}
-
-        .tab_item {
-        width: calc(100%/3);
-        height: 50px;
-        border-bottom: 3px solid #5ab4bd;
-        background-color: #d9d9d9;
-        line-height: 50px;
-        font-size: 16px;
-        text-align: center;
-        color: #565656;
-        display: block;
-        float: left;
-        text-align: center;
-        font-weight: bold;
-        transition: all 0.2s ease;
-        }
-
-        .tab_item:hover {
-        opacity: 0.75;
-        }
-
-        input[name="tab_item"] {
-        display: none;
-        }
-
-        .tab_content {
-        display: none;
-        padding: 40px 40px 0;
-        clear: both;
-        overflow: hidden;
-        }
-
-        #class:checked ~ #class_content,
-        #teacher:checked ~ #teacher_content,
-        #design:checked ~ #design_content {
-        display: block;
-        }
-
-        .tabs input:checked + .tab_item {
-        background-color: #5ab4bd;
-        color: #fff;
-        }
-    </style>
 </head>
 
 <body>
@@ -68,38 +18,59 @@
 
         <div class="tab_content" id="class_content">
             <div class="tab_content_description">
-                <input v-model="keyword" placeholder="search..." type="text">
+                <div class="cp_iptxt">
+                    <label class="ef">
+                        <input type="text"
+                            v-model="keyword"
+                            placeholder="Search by official name and you will get a hit (ex: Spoken English ...)"
+                            type="text">
+                    </label>
+                </div>
                 <div v-for="data in filterData">
-                    <a :href="data.url">
-                        <p>@{{data.time}}</p>
-                        <p>@{{data.subject}}</p>
+                    <a :href="data.url" target="_blank">
+                        <p class="data_para">@{{data.time}} @{{data.subject}}</p>
                     </a>
                 </div>
             </div>
         </div>
         <div class="tab_content" id="teacher_content">
             <div class="tab_content_description">
-                <input v-model="keyword" placeholder="search..." type="text">
+                <div class="cp_iptxt">
+                    <label class="ef">
+                        <input type="text"
+                            v-model="keyword"
+                            placeholder="Search by official name and you will get a hit (ex: Spoken English ...)"
+                            type="text">
+                    </label>
+                </div>
                 <div v-for="data in filterData">
-                    <a :href="data.url">
-                        <p>@{{data.subject}}</p>
-                        <p>@{{data.teacher}}</p>
+                    <a :href="data.url" target="_blank">
+                        <p class="data_para">@{{data.subject}} @{{data.teacher}}</p>
                     </a>
                 </div>
             </div>
         </div>
         <div class="tab_content" id="design_content">
             <div class="tab_content_description">
-                <input v-model="keyword" placeholder="search..." type="text">
+                <div class="cp_iptxt">
+                    <label class="ef">
+                        <input type="text"
+                            v-model="keyword"
+                            placeholder="Search by official name and you will get a hit (ex: Spoken English ...)"
+                            type="text">
+                    </label>
+                </div>
                 <div v-for="data in filterData">
-                    <a :href="data.url">
-                        @{{data.time}}
-                        @{{data.subject}}
-                        @{{data.teacher}}
-                        @{{data.class}}
-                        @{{data.url}}
-                        @{{data.semester}}
-                        @{{data.credit}}
+                    <a :href="data.url" target="_blank">
+                        <p class="data_para">
+                            @{{data.time}}
+                            @{{data.subject}}
+                            @{{data.teacher}}
+                            @{{data.class}}
+                            @{{data.url}}
+                            @{{data.semester}}
+                            @{{data.credit}}
+                        <p>
                     </a>
                 </div>
             </div>
