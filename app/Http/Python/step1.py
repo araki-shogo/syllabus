d11a086bf8fb3c92cdcde65c07a919a31977669d
araki_shogo <araki@crane-inc.io>
2021-01-16 23:41:33 +0900

    step1 done

--- app/Http/Python/step1.py
+++ app/Http/Python/step1.py
@@ -1,21 +1,24 @@
 import os
+import sys
+sys.path.append('C:\\Python38\\lib\\site-packages')
 from pathlib import Path
 from pdf2image import convert_from_path
 
 # poppler/binを環境変数PATHに追加する
 #poppler_dir = Path(__file__).parent.absolute() / "poppler/bin"
-poppler_dir = "/usr/local/Cellar/poppler/21.01.0/bin"
+poppler_dir = "C:\\Program Files\\poppler-0.67.0\\bin"
 os.environ["PATH"] += os.pathsep + str(poppler_dir)
 
 # PDFファイルのパス
 #pdf_path = Path("./pdf_file/pdf3009p.pdf")
-pdf_path = Path("./pdf/jikanwari_syllabus.pdf")
+this_path = os.getcwd() + "\\app\\Http\\Python\\"
+pdf_path = Path(this_path + "jikanwari_syllabus.pdf")
 
 # PDF -> Image に変換（600dpi）
 pages = convert_from_path(str(pdf_path), 600)
 
 # 画像ファイルを１ページずつ保存
-image_dir = Path("./step1out") # 事前にstep1outという保存フォルダが必要
+image_dir = Path(this_path + "step1out")  # 事前にstep1outという保存フォルダが必要 *
 for i, page in enumerate(pages):
     file_name = pdf_path.stem + "_{:02d}".format(i + 1) + ".jpeg"
     image_path = image_dir / file_name
